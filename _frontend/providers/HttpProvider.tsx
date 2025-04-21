13da7e2bc04f7b3e2ffd43a2b9510117ed3eb174
Aiman Azani <m.aimanazani@gmail.com>
2025-04-22 06:07:21 +0800

    upgrade version

--- _frontend/providers/HttpProvider.tsx
+++ _frontend/providers/HttpProvider.tsx
@@ -1,23 +1,14 @@
 "use client";
 
-import { LOGIN_ROUTE } from "@/constants/route";
 import {
   QueryCache,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { AxiosError } from "axios";
 
-import { usePathname, useRouter } from "next/navigation";
 import { PropsWithChildren, useState } from "react";
-import { useCookies } from "react-cookie";
 
 const HttpProvider = ({ children }: PropsWithChildren) => {
-  const [, , removeCookie] = useCookies(["x-token"]);
-  const pathname = usePathname();
-  const router = useRouter();
-
   const [client] = useState(
     () =>
       new QueryClient({
@@ -33,27 +24,14 @@ const HttpProvider = ({ children }: PropsWithChildren) => {
           },
         },
 
-        queryCache: new QueryCache({
-          onError: (error) => {
-            const axiosError = error as AxiosError;
-
-            if (axiosError.response?.status === 401) {
-              removeCookie("x-token", { path: "/" });
-              let redirect = LOGIN_ROUTE;
-              if (pathname !== "/") {
-                redirect += `?next=${pathname}`;
-              }
-              router.replace(redirect);
-            }
-          },
-        }),
+        queryCache: new QueryCache({}),
       })
   );
 
   return (
     <QueryClientProvider client={client}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" />
+      {/* <ReactQueryDevtools initialIsOpen={false} buttonPosition="top-left" /> */}
     </QueryClientProvider>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "rizzui";
 
 export default function Home() {
